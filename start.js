a8cca0c8d017106580cc07daeea25f2634fc4a7f
George Brightman <68116400+GBrightman@users.noreply.github.com>
2022-08-12 03:29:44 +0100

    Update start.js

--- start.js
+++ start.js
@@ -17,3 +17,21 @@ function getScore() {
 function returnScore() {
   document.getElementById("score").innerHTML = getScore() + "/" + total; // prints the score to an element on the page
 }
+
+// When the user scrolls the page, execute scrollNavbar
+window.onscroll = function() {scrollNavbar()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function scrollNavbar() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
